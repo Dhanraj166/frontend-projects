66e13e518c21b37251046b66b1bdf78c2c0e94f2
Dhanraj166 <dhanrajs781004@gmail.com>
2026-02-21 20:15:41 +0530

    complete the code

--- armstrong.js
+++ armstrong.js
@@ -1,5 +1,18 @@
 let n=371;
 let count=0;
+let ams=0;
 while(n>0){
-    
+    n=Math.floor(n/10);
+    count++
+}
+while(n>0){
+    let digit=n%10;
+    ams=digit**count;
+    n=math.floor(n/10);
+}
+
+if(n===ams){
+    console.log("Armstrong Number")
+}else{
+    console.log("Not Armstrong Number")
 }
