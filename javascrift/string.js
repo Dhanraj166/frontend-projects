c1f7d3e521a5d6c72d516b217879fe6ca0fa27bc
Dhanraj166 <dhanrajs781004@gmail.com>
2026-03-06 23:03:50 +0530

    practice

---
+++ javascrift/string.js
@@ -0,0 +1,45 @@
+// let price = 10;
+// let VAT = 0.25;
+
+// let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+// console.log(total);
+
+
+// let header = "Template Strings";
+// let tags = ["template strings", "javascript", "es6"];
+
+// let html = `<h2>${header}</h2><ul>`;
+// for (const x of tags) {
+//   html += `<li>${x}</li>`;
+// }
+
+// html += `</ul>`;
+// console.log(html);
+
+// let text = "HELLO WORLD";
+// let char = text.charAt(text.length/2+1);
+// console.log(char);
+
+
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat(" ",text2);
+// console.log(text3);
+
+
+// let text4 = "Apple, Banana, Kiwi";
+// let part = text4.slice(7, 13);
+// console.log(text4);
+// console.log(part);
+
+
+
+// let text = "Hello World \uD800";
+// let result = text.toWellFormed();
+// console.log(result);
+
+
+let text = "javascript";
+for(i=0;i<length;i++){
+    if(i)
+}
